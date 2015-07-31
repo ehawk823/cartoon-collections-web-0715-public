0057878bbb0dc20f0339ec5db31fe2d664e81bc9
ehawk823 <evan.alexander.hawk@gmail.com>
2015-07-31 18:04:13 -0500

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each do |dwarf|
+    puts "#{dwarves.index(dwarf) + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(veggies)
+  new_array = []
+  veggies.collect do |item|
+    new_array.push(item.capitalize + "!")
+  end
+  new_array
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(call)
+  call.each do |item|
+    if item.length > 4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
+  array = []
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each do |item|
+    if cheese_types.include?(item)
+      array.push(item)
+    end
+  end
+  return array[0]
 end
